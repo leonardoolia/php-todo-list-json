45e4ead4a0e6d7c9dc61c6b2784ffd6bee9957c9
leonardoolia <leonardooliaa@gmail.com>
2024-02-24 15:15:04 +0100

    created data tasks

---
+++ app/js/script.js
@@ -0,0 +1,32 @@
+const { createApp } = Vue;
+
+const app = createApp({
+    name: 'Todo List',
+    data: () => ({
+        tasks: [
+            {
+                "id": 1,
+                "text": "HTML",
+                "done": false
+            },
+            {
+                "id": 2,
+                "text": "CSS",
+                "done": false
+            },
+            {
+                "id": 3,
+                "text": "Responsive design",
+                "done": false
+            },
+            {
+                "id": 4,
+                "text": "Javascript",
+                "done": false
+            }
+        ]
+    })
+
+})
+
+app.mount('#app')
