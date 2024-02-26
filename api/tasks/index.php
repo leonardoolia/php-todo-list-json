aaa8f40e6370e23ade73ef31b63cd603a1dcd0f1
leonardoolia <leonardooliaa@gmail.com>
2024-02-26 15:38:01 +0100

    got tasks by using axios

--- api/tasks/index.php
+++ api/tasks/index.php
@@ -3,6 +3,8 @@ $string = file_get_contents('../../database/tasks.json');
 
 $tasks = json_decode($string, true);
 
+
+
 header('Content-Type: application/json');
 
 echo json_encode($tasks);
--- app/js/script.js
+++ app/js/script.js
@@ -1,31 +1,19 @@
 const { createApp } = Vue;
 
+const endpoint = 'http://localhost/Boolean/php-todo-list-json/api/tasks/index.php';
+
 const app = createApp({
     name: 'Todo List',
     data: () => ({
-        tasks: [
-            {
-                "id": 1,
-                "text": "HTML",
-                "done": false
-            },
-            {
-                "id": 2,
-                "text": "CSS",
-                "done": false
-            },
-            {
-                "id": 3,
-                "text": "Responsive design",
-                "done": false
-            },
-            {
-                "id": 4,
-                "text": "Javascript",
-                "done": false
-            }
-        ]
-    })
+        tasks: []
+    }),
+    created() {
+        axios.get(endpoint).then(res => {
+            this.tasks = res.data
+        })
+    }
+
+
 
 })
 
