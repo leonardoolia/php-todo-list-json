5180384a53f4a7d4b89c13aa59a4c48a7bb3b9a9
leonardoolia <leonardooliaa@gmail.com>
2024-02-28 15:41:27 +0100

    added method to add tasks

--- api/tasks/index.php
+++ api/tasks/index.php
@@ -8,3 +8,4 @@ $tasks = json_decode($string, true);
 header('Content-Type: application/json');
 
 echo json_encode($tasks);
+
--- app/js/script.js
+++ app/js/script.js
@@ -5,8 +5,21 @@ const endpoint = 'http://localhost/Boolean/php-todo-list-json/api/tasks/index.ph
 const app = createApp({
     name: 'Todo List',
     data: () => ({
-        tasks: []
+        tasks: [],
+        newTask: ''
     }),
+    methods: {
+        addTask() {
+            if (this.newTask.trim() !== '') {
+                this.tasks.push({
+                    id: this.tasks.length + 1,
+                    text: this.newTask,
+                    done: false,
+                });
+                this.newTask = '';
+            }
+        }
+    },
     created() {
         axios.get(endpoint).then(res => {
             this.tasks = res.data
